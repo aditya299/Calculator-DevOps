035a64c9c1031aa20bbb7596062d5c5026c1b4e0
aditya299 <adityadahasahasra@gmail.com>
2020-05-01 11:16:19 +0530

    Added addition with tests

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,26 @@
+package com.calculator;
+
+/**
+ * Hello world!
+ *
+ */
+//public class App 
+//{
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+//}
+
+
+//package com.calculator;
+
+public class Calculator{
+	public static void main(String[] args) {
+		System.out.println("9 + 7 = " + add(9, 7));
+	}
+	
+	public static int add(int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/test/java/com/calculator/CalculatorTests.java
@@ -0,0 +1,17 @@
+package com.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTests {
+
+	@Test
+	public void Addtest() {
+		int a = 45;
+		int b = 45;
+		int result = Calculator.add(a, b);
+		assertEquals(90, result);
+	}
+
+}
