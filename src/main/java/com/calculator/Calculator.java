2e2a4cbd71c8bcf7ff043a7dba0c9e7ee28bc78f
aditya299 <adityadahasahasra@gmail.com>
2020-05-01 12:18:11 +0530

    Third add commit

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -17,7 +17,7 @@ package com.calculator;
 
 public class Calculator{
 	public static void main(String[] args) {
-		System.out.println("9 + 7 = " + add(10, 7));
+		System.out.println("9 + 7 = " + add(106, 7));
 	}
 	
 	public static int add(int a, int b) {
