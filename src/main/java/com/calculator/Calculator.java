42e7d5a3b5b5dbcc498f8c4d984aecf4bb8da4df
aditya299 <adityadahasahasra@gmail.com>
2020-05-03 17:42:09 +0530

    Added subtract funtionality

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -17,10 +17,15 @@ package com.calculator;
 
 public class Calculator{
 	public static void main(String[] args) {
-		System.out.println("9 + 7 = " + add(1066, 7));
+		System.out.println("1066 + 7 = " + add(1066, 7));
+		System.out.println("5667 - 234 = " + subtract(5667, 234));
 	}
 	
 	public static int add(int a, int b) {
 		return a + b;
+	}
+	
+	public static int subtract(int a, int b) {
+		return a - b;
 	}
 }
---
+++ src/test/java/com/calculator/CalculatorSubTest.java
@@ -0,0 +1,17 @@
+package com.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorSubTest {
+
+	@Test
+	public void SubTest() {
+		int a = 234;
+		int b = 100;
+		int result = Calculator.subtract(a, b);
+		assertEquals(134, result);
+	}
+
+}
