6485b6fa64f256c51144abf755582eb5f4cbdf1b
aditya299 <adityadahasahasra@gmail.com>
2020-05-09 16:59:40 +0530

    Final commit

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,4 +1,4 @@
-package com;
+package com.calculator;
 
 public class Calculator{
 	public static void main(String[] args) {
--- src/test/java/com/calculator/subTest.java
+++ src/test/java/com/calculator/subTest.java
@@ -1,4 +1,4 @@
-package com;
+package com.calculator;
 
 import static org.junit.Assert.*;
 
